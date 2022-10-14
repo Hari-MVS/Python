b6937e07731e22c4ac96c9b2b3c16fb6a96e7710
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-10-14 07:17:48 +0530

    Create List_indexing_2.py

---
+++ NXT_WAVE_INTENSIVE/Lists/Coding_practice_19/List_indexing_2.py
@@ -0,0 +1,5 @@
+programming_languages_list = ["Python", "Java", "Ruby", "C", "C++", "Go", "R", "JavaScript", "Swift", "PHP", "Kotlin", "Perl"]
+a =int(input())
+for i in range(a):
+    n=int(input())
+    print(programming_languages_list[n])
