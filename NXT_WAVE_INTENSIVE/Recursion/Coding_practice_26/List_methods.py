e127cac2bbba30637b0ed8f056f4584de1922de0
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-10-21 08:54:34 +0530

    Create List_methods.py

---
+++ NXT_WAVE_INTENSIVE/Recursion/Coding_practice_26/List_methods.py
@@ -0,0 +1,16 @@
+n= int(input())
+l=[]
+for i in range(n):
+    m=input().split()
+    if m[0] == 'insert':
+        l.insert(int(m[1]),int(m[2]))
+    elif m[0] == 'append':
+        l.append(int(m[1]))
+    elif m[0] == 'pop':
+        l.pop()
+    elif m[0] == "remove":
+        l.remove(int(m[1]))
+    elif m[0] == "sort":
+        l.sort()
+    elif m[0] == "print":
+        print(l)
