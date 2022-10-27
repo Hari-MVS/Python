d46166209f00fb2b7473a25f48001c9b196ea95a
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-10-27 08:06:51 +0530

    Create Row_Minimum_maximum_and_sum.py

---
+++ NXT_WAVE_INTENSIVE/Dictionaries/Coding_practice_30/Row_Minimum_maximum_and_sum.py
@@ -0,0 +1,30 @@
+def print_max_min_sum_for_row_wise(num_list):
+    max_val=[]
+    min_val=[]
+    sum_val=[]
+    for i in num_list:
+        max_val.append(max(i))
+        min_val.append(min(i))
+        sum_val.append(sum(i))
+    print(max_val)
+    print(min_val)
+    print(sum_val)
+
+def convert_string_to_int(list_a):
+    new_list = []
+    for item in list_a:
+        num = int(item)
+        new_list.append(num)
+    return new_list
+
+
+m, n = input().split()
+m, n = int(m), int(n)
+num_list = []
+
+for i in range(m):
+    list_a = input().split()
+    list_a = convert_string_to_int(list_a)
+    num_list.append(list_a)
+
+print_max_min_sum_for_row_wise(num_list)
