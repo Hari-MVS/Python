db3761f7236f96b671e9ea8078104669378d148d
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-10-28 09:23:25 +0530

    Create Update_a_value_key.py

---
+++ NXT_WAVE_INTENSIVE/Dictionaries/Coding_practice_31/Update_a_value_key.py
@@ -0,0 +1,12 @@
+students_dict = {
+    "Ram": "Cricket",
+    "Naresh": "Football",
+    "Vani": "Tennis",
+    "Rahim": "Cricket",
+    "Deepak": "Boxing"
+}
+
+a,b=input().split()
+students_dict[a]=b
+print(students_dict)
+
