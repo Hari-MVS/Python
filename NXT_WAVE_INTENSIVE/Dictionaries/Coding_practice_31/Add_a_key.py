2e868edfcb64afc57c747febcbb28a22a08aa90b
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-10-28 09:22:57 +0530

    Create Add_a_key.py

---
+++ NXT_WAVE_INTENSIVE/Dictionaries/Coding_practice_31/Add_a_key.py
@@ -0,0 +1,10 @@
+students_dict = {
+    "Ram": "Cricket",
+    "Naresh": "Football",
+    "Vani": "Tennis",
+    "Rahim": "Cricket"
+}
+
+a=input().split()
+students_dict[a[0]]=a[1]
+print(students_dict)
