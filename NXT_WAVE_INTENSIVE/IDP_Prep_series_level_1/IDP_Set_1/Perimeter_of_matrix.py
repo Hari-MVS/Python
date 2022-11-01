1ac4cb54cd03a3555777cd24e8921cca61f55fbf
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-11-01 15:09:44 +0530

    Create Perimeter_of_matrix.py

---
+++ NXT_WAVE_INTENSIVE/IDP_Prep_series_level_1/IDP_Set_1/Perimeter_of_matrix.py
@@ -0,0 +1,17 @@
+r,c=input().split()
+r=int(r)
+c=int(c)
+mat=[]
+for i in range(r):
+    temp=[]
+    for j in input().split():
+        temp.append(int(j))
+    mat.append(temp)
+
+summ=0      
+for i in range(r):
+    for j in range(c):
+        if i==0 or i==r-1 or j==0 or j==c-1:
+            summ+=mat[i][j]
+print(summ)
+    
